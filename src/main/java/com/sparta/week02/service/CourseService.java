c076bb39311ecf41e431c7a7befec7e5a6fa97c0
naracodes <naralee0124@gmail.com>
2020-12-25 13:42:53 -0500

    apply DTO to encapsulate data

--- src/main/java/com/sparta/week02/service/CourseService.java
+++ src/main/java/com/sparta/week02/service/CourseService.java
@@ -2,6 +2,7 @@ package com.sparta.week02.service;
 
 import com.sparta.week02.domain.Course;
 import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +21,11 @@ public class CourseService {
 //    } // Processable via @RequiredArgsConstructor instead!
 
     @Transactional // SQL 쿼리가 일어나야 함을 스프링에게 알려줌
-    public Long update(Long id, Course course) {
+    public Long update(Long id, CourseRequestDto requestDto) {
         Course course1 = courseRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
         );
-        course1.update(course);
+        course1.update(requestDto);
         return course1.getId();
     }
 }
